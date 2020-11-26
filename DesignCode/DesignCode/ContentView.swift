91f9bf22a96439833c18f805bcfb2ad7616641df
Iñaki Narciso <ionicsoft@gmail.com>
2020-11-26 22:20:00 +0800

    Add back card component

--- DesignCode/DesignCode/ContentView.swift
+++ DesignCode/DesignCode/ContentView.swift
@@ -10,6 +10,20 @@ import SwiftUI
 struct ContentView: View {
   var body: some View {
     ZStack {
+      BackCardView(
+        offset: CGPoint(x: 0, y: -40),
+        backgroundColor: Color("card4"),
+        scaleFactor: 0.9,
+        rotationDegree: 10
+      )
+
+      BackCardView(
+        offset: CGPoint(x: 0, y: -20),
+        backgroundColor: Color("card3"),
+        scaleFactor: 0.95,
+        rotationDegree: 5
+      )
+
       CardView()
     }
   }
@@ -48,6 +62,32 @@ struct CardView: View {
     .background(Color.black)
     .cornerRadius(20)
     .shadow(radius: 20)
+    .blendMode(.hardLight)
+  }
+}
+
+struct BackCardView: View {
+  
+  var offset: CGPoint = .zero
+  var backgroundColor: Color = .blue
+  var scaleFactor: CGFloat = 1.0
+  var rotationDegree: Double = .zero
+
+  var body: some View {
+    VStack {
+      Spacer()
+    }
+    .frame(width: 340, height: 220)
+    .background(backgroundColor)
+    .cornerRadius(20)
+    .shadow(radius: 20)
+    .offset(x: offset.x, y: offset.y)
+    .scaleEffect(scaleFactor)
+    .rotationEffect(.degrees(rotationDegree))
+    .rotation3DEffect(
+      .degrees(rotationDegree),
+      axis: (x: CGFloat(rotationDegree), y: 0.0, z: 0.0)
+    )
     .blendMode(.hardLight)
   }
 }

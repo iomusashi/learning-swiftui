81511a924a19eb68291e9a54430a2e8619efe522
Iñaki Narciso <ionicsoft@gmail.com>
2020-11-26 22:19:14 +0800

    Add card component

--- DesignCode/DesignCode/ContentView.swift
+++ DesignCode/DesignCode/ContentView.swift
@@ -9,13 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    ZStack {
+      CardView()
+    }
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
+  }
+}
+
+struct CardView: View {
+  var body: some View {
+    VStack {
+      HStack {
+        VStack(alignment: .leading) {
+          Text("UI Design")
+            .font(.title)
+            .fontWeight(.semibold)
+            .foregroundColor(.white)
+          Text("Certificate")
+            .foregroundColor(Color("accent"))
+        }
+        Spacer()
+        Image("Logo1")
+      }
+      .padding()
+      
+      Spacer()
+      Image("Card1")
+        .resizable()
+        .aspectRatio(contentMode: .fill)
+        .frame(width: 300, height: 110, alignment: .top)
+    }
+    .frame(width: 340, height: 220)
+    .background(Color.black)
+    .cornerRadius(20)
+    .shadow(radius: 20)
+    .blendMode(.hardLight)
   }
 }

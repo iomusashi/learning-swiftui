c231587ab7d04057850b46bbc9a92a440bd2e25d
Iñaki Narciso <ionicsoft@gmail.com>
2020-11-26 21:08:32 +0800

    Initial commit

---
+++ DesignCode/DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by インヤキ on 11/26/20.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
